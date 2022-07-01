8fa9bf8151c6930f5397aa0bb640e3cf566a71cf
Hadi Razzak <hadi_razzak@huntsman.com>
2022-07-01 14:07:50 -0500

    Initial Commit

---
+++ CICD/ViewController.swift
@@ -0,0 +1,31 @@
+//
+/*
+ ********************************************************************************
+ ************************ HUNTSMAN INTERNATIONAL LLC ****************************
+ ********************** Headquarters: The Woodlands, TX *************************
+ ********************************************************************************
+ * Author                : Bumpy
+ * Created On            : 7/1/22
+ * Project Name          : CICD
+ * Huntsman Change Number:
+ * Bespoke Number        :
+ * Project Description   :
+ *                       :
+ *                       :
+ *
+ * 
+ ********************************************************************************
+*/
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
